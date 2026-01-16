ac08537b2f2a62e771192b7c721b9b8f278c36c5
anusha <anushakulkarni007@gmail.com>
2026-01-16 20:42:18 +0530

    saved

---
+++ celi.py
@@ -0,0 +1,9 @@
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        celsius = float(sys.argv[1])
+        print("USER INPUT PROVIDED")
+    else:
+        celsius = 25.0
+print("Fahrenheit: ", celsius_to_fahrenheit(celsius))            
